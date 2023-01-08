39087bcdbd8a3eb4a6449408ddf349b1dbfc9fed
mathyouf <11fishem@gmail.com>
2023-01-08 09:51:58 -0800

    wandbloggin

--- train.py
+++ train.py
@@ -53,14 +53,14 @@ class ImagePredictionLogger(pl.Callback):
 
         # Log the images - download url
          # Get image pairs and ranks
-        table = wandb.Table(columns=['img1', 'img2', 'rank', 'label'])
+        table = wandb.Table(columns=['img1', 'img2', 'pred', 'label'])
         for i in range(len(imgs1)):
             img1 = wandb.Image(imgs1[i], caption=caption1[i])
             img2 = wandb.Image(imgs2[i], caption=caption2[i])
-            y = y.detach().cpu().item()
+            y = y.detach().cpu()[i]
             # Convert to int
-            y_hat = y_hat.detach().cpu().item()
-            table.add_data(img1, img2, y, y_hat)
+            y_hat = y_hat.detach().cpu()[i]
+            table.add_data(img1, img2, y_hat, y)
 
         # Log the table
         trainer.logger.experiment.log({
